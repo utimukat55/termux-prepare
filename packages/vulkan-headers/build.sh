11c2b6732883995470d4086ffbd234eac40e1b3b
worker <worker@localhost>
2026-05-02 14:43:29 +0900

    upstream as of 202605021142

---
+++ packages/vulkan-headers/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/KhronosGroup/Vulkan-Headers
+TERMUX_PKG_DESCRIPTION="Vulkan Header files and API registry"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1.4.350"
+TERMUX_PKG_SRCURL=https://github.com/KhronosGroup/Vulkan-Headers/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=6dd105e5cc7ddab6e7b611ae2c1872740d1727557cc8bf9daf13d6de1e4b3999
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
+TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
