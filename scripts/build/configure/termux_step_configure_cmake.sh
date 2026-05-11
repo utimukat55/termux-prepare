caefdbe971dacdc7ea87f8702e4044ef3c97b38f
admin <admin@localhost>
2026-05-11 19:46:47 +0900

    add verbose ninja

--- scripts/build/configure/termux_step_configure_cmake.sh
+++ scripts/build/configure/termux_step_configure_cmake.sh
@@ -55,6 +55,7 @@ termux_step_configure_cmake() {
 		-DBUILD_TESTING=OFF \
 		"${CMAKE_ADDITIONAL_ARGS[@]}" \
 		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS \
+		-v \
 		|| (termux_step_configure_cmake_failure_hook && false)
 }
 
