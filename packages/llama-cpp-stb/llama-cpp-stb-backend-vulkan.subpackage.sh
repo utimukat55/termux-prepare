cc97783cb5980b83f53620ad87a579d8d4bd2ba8
root <root@localhost>
2026-05-20 15:43:42 +0900

    add patches/change workflow/add conflicts

--- packages/llama-cpp-stb/llama-cpp-stb-backend-vulkan.subpackage.sh
+++ packages/llama-cpp-stb/llama-cpp-stb-backend-vulkan.subpackage.sh
@@ -1,6 +1,7 @@
 TERMUX_SUBPKG_DESCRIPTION="Vulkan Backend for LLaMA.cpp"
 TERMUX_SUBPKG_RECOMMENDS="vulkan-loader"
 TERMUX_SUBPKG_SUGGESTS="vulkan-loader-generic, mesa-vulkan-icd-freedreno"
+TERMUX_SUBPKG_CONFLICTS="llama-cpp-backend-vulkan"
 TERMUX_SUBPKG_INCLUDE="
 lib/libggml-vulkan.so
 "
